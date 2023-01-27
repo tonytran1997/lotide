96747c1e9d56db6256a9a02ac80f56836032e978
Tony Tran <tonytran1645@gmail.com>
2023-01-26 22:32:10 -0500

    first commit

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+console.assert(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
